fda857b9143a0e0f92a336e568f8b5eec49d11b3
Eric Ge <ericlege@berkeley.edu>
2018-11-05 23:54:16 -0800

    map

---
+++ Safe2HomeIOS/Safe2HomeIOS/HomeMapView.swift
@@ -0,0 +1,23 @@
+//
+//  HomeMapView.swift
+//  Safe2HomeIOS
+//
+//  Created by Eric Le Ge on 11/5/18.
+//  Copyright © 2018 Safe2Home. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class HomeMapView: MKMapView {
+    var current_location: CLLocation?
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
