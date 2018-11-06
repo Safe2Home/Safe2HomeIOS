70464d91360753a90babd3c2d95d313b0acd516e
Eric Ge <ericlege@berkeley.edu>
2018-11-05 20:19:15 -0800

    test

---
+++ Safe2HomeIOS/Safe2HomeIOS/Location.swift
@@ -0,0 +1,12 @@
+//
+//  File.swift
+//  Safe2HomeIOS
+//
+//  Created by Eric Le Ge on 11/5/18.
+//  Copyright © 2018 Safe2Home. All rights reserved.
+//
+
+import Foundation
+import CoreLocation
+
+let global_manager = CLLocationManager()
